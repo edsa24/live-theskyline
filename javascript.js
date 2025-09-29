3a9455ab89608257d16587d7eae3f23f440d11ce
edsa24 <160583209+edsa24@users.noreply.github.com>
2025-09-30 00:39:11 +0800

    Delete javascript.js

--- javascript.js
+++
@@ -1,6 +0,0 @@
-const menuIcon = document.getElementById("menu-icon");
-const navLinks = document.querySelector("nav ul");
-
-menuIcon.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
-});
